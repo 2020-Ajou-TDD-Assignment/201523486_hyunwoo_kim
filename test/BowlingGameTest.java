829e1515d21be7ad55ed026f6cca830585872935
hwkim <lusist@ajou.ac.kr>
2020-05-08 15:50:02 +0900

    third test commit

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -1,5 +1,5 @@
 import static org.junit.Assert.*;
-
+import org.junit.Ignore;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,5 +22,14 @@ public class BowlingGameTest {
 	public void testAllOnes(){
 		rollMany(20, 1);
 		assertEquals(20, g.score());
+	}
+	@Ignore("until we get design right")
+	@Test
+	public void testOneSpare() {
+		g.roll(5);
+		g.roll(5); // spare
+		g.roll(3);
+		rollMany(17,0);
+		assertEquals(16,g.score());
 	}
 }
